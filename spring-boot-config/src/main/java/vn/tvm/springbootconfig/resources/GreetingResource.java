71cd11739994c4aa094d71938bb95407f5169863
Tran Tuan Minh Vu <tranvu.ff@gmail.com>
2022-02-17 17:50:03 +0700

    add the environment

--- spring-boot-config/src/main/java/vn/tvm/springbootconfig/resources/GreetingResource.java
+++ spring-boot-config/src/main/java/vn/tvm/springbootconfig/resources/GreetingResource.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,24 +17,34 @@ public class GreetingResource {
     @Value("${conf.greeting: This is default value}")
     private String greetingMessage;
     
-    @Value("${conf.app-description}")
+    @Value("${conf.description:}")
     private String appDescription;
     
     @Value("This is the static message")
     private String staticMessage;
     
-    @Value("${conf.app-list-values}")
+    @Value("${conf.listValues:}")
     private List<String> values;
     
-    @Value("#{${conf.values}}")
+    @Value("#{${conf.values:}}")
     private Map<String, String> mapValues;
     
     @Autowired
     private DbSetting dbSetting;
     
+    @Autowired
+    private Environment env;
+    
     @GetMapping("/greeting")
     public String greeting() {
         return greetingMessage + " - " + appDescription + " - " + staticMessage + " - "
                 + values + " - " + mapValues + " - " + dbSetting.getConnection();
+    }
+    
+    @GetMapping("/env-details")
+    public String getEnvDetails() {
+        // Don't look up profiles -> affects testability
+        // Don't look up properties -> Should use @Value and ${}
+        return env.toString();
     }
 }
