06be353e7c8116793a2f30ef71e241840dd7dc14
Tran Tuan Minh Vu <tranvu.ff@gmail.com>
2022-02-19 14:39:19 +0700

    Adding configuration server - client

---
+++ spring-cloud-config-server/src/test/java/vn/tvm/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tvm.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
